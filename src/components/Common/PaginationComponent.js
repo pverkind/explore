1d181aac77f652cdd6c10d01313f10ce115fa831
zisanurhaque <zisanurhaqueag@gmail.com>
2024-01-30 23:13:18 +0600

    latest code added

---
+++ src/components/Common/PaginationComponent.js
@@ -0,0 +1,36 @@
+import { TablePagination } from "@mui/material";
+import React from "react";
+import { useContext } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Context } from "../../App";
+
+const PaginationComponent = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { totalRecords, rowsPerPage, page } = useContext(Context);
+
+  // change prev and next page from pagination bar
+  const handleChangePage = async (newPage) => {
+    const params = Object.fromEntries([...searchParams]);
+    setSearchParams({ ...params, page: newPage });
+  };
+
+  // change rows per page from pagination dropdown
+  const handleChangeRowsPerPage = (event) => {
+    const params = Object.fromEntries([...searchParams]);
+    setSearchParams({ ...params, rowsPerPage: +event.target.value });
+  };
+
+  return (
+    <TablePagination
+      rowsPerPageOptions={[10, 25, 100]}
+      component="div"
+      count={totalRecords}
+      rowsPerPage={rowsPerPage}
+      page={page - 1}
+      onPageChange={(e, pageNumber) => handleChangePage(pageNumber + 1)}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+    />
+  );
+};
+
+export default PaginationComponent;
